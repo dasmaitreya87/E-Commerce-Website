9877af1c7ff9276ba6d2fdc52899e43b0ff0f3fc
Maitreya Das <dasmaitreya87@gmail.com>
2025-08-13 23:46:05 +0530

    Final changes

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import Navbar from './components/Navbar'
-import Sidebar from './components/Sidebar'
+import Sidebar from './components/Sidebar' 
 import { Route, Routes } from 'react-router-dom'
 import Add from './pages/Add'
 import List from './pages/List'
